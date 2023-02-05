740abad674f78536472db91528290b92cd7015f3
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-02-05 18:01:27 +0900

    항목 추가 기능 추가

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,7 @@
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
-import { useState } from "react";
+import { useState, useCallback, useRef } from "react";
 
 const App = () => {
   const [todos, setTodos] = useState([
@@ -22,9 +22,23 @@ const App = () => {
     },
   ]);
 
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert></TodoInsert>
+      <TodoInsert onInsert={onInsert}></TodoInsert>
       <TodoList todos={todos}></TodoList>
     </TodoTemplate>
   );
--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,10 +1,31 @@
 import { MdAdd } from "react-icons/md";
+import { useState, useCallback } from "react";
 import "./TodoInsert.scss";
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState("");
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(""); //value 값 초기화
+      e.preventDefault(); // submit 이벤트는 브라우저에서 새로고침을 발생시키기때문에 이를 방지하기 위한 함수 호출, 설정해주지 않으면 새로고침이 되면서 초기화된 Value가 설정된다.
+    },
+    [onInsert, value]
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요"></input>
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
+
       <button type="submit">
         <MdAdd></MdAdd>
       </button>
